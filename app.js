3e920770b5f5cf12f945a3f5105a88797420c09d
mohamed ahmed elhalwagui <73799728+ElhalwaguiAhmedMohamed@users.noreply.github.com>
2020-12-27 21:30:02 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener("click",function(){
+    //get random random number between 0-3
+    let randomNumber = getRamdomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRamdomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
